c123ab91b978f1bd40ff8fd98356db29019e7728
Jessiesaurus1706 <jessicaarendonb@hotmail.com>
2026-07-14 18:15:41 -0500

    feat: ajuste

--- src/presentation/components/ui/Header/Header.tsx
+++ src/presentation/components/ui/Header/Header.tsx
@@ -1,15 +1,12 @@
 import styles from './Header.module.scss';
+import Logo from '../../../../assets/images/header-logo.svg';
 
-interface HeaderProps {
-  logo: string;
-}
-
-const Header = ({ logo }: HeaderProps) => {
+const Header = () => {
   return (
     <header className={styles.header}>
       <img
-        src={logo}
-        alt="Logo"
+        src={Logo}
+        alt="Crédito Fácil"
         className={styles.logo}
       />
     </header>
